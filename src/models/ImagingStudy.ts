9d9664fa968fd5c1cf7afda42c254f081b436cb5
Jaak <jaak@Jaaks-MacBook-Pro.local>
2026-07-08 18:12:33 +0300

    Add imaging data model

---
+++ src/models/ImagingStudy.ts
@@ -0,0 +1,29 @@
+import { Visibility } from "@/models/Visibility";
+
+export type ImagingStatus =
+  | "processing"
+  | "available"
+  | "viewed";
+
+export type ImagingModality =
+  | "XR"
+  | "CT"
+  | "US"
+  | "ECG"
+  | "OTHER";
+
+export type ImagingStudy = {
+  id: string;
+  exerciseId: string;
+  patientId: string;
+
+  modality: ImagingModality;
+  title: string;
+  report: string;
+  attachment?: string;
+
+  status: ImagingStatus;
+  visibility: Visibility;
+
+  releasedAt?: string;
+};
